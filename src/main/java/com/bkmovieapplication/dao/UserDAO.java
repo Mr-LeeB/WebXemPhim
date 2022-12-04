32750f7b01ba982a0941e014ef1e56289dd67d24
Mr-LeeB <quangbaokh9a2@gmail.com>
2022-12-04 16:43:08 +0700

    update

--- src/main/java/com/bkmovieapplication/dao/UserDAO.java
+++ src/main/java/com/bkmovieapplication/dao/UserDAO.java
@@ -13,16 +13,19 @@ public class UserDAO {
     ResultSet rs = null;
 
     public void create(String username, String email, String password, String phonenum) {
-        String query = "insert into oj3bOO0Agn.user\n"
-                + "values(?,?,?,?,?)";
+        String query = "insert into oj3bOO0Agn.user (	\n"
+                + "	userName,\n"
+                + "    email,\n"
+                + "    passWord,\n"
+                + "    phoneNum)\n"
+                + "values('?','?','?','?')";
         try {
-            conn = new DBContext().getConnection();//mo ket noi voi sql
+            conn = new DBContext().getConnection();
             ps = conn.prepareStatement(query);
-            ps.setInt(1, 3);
-            ps.setString(2, username);
-            ps.setString(3, email);
-            ps.setString(4, password);
-            ps.setString(5, phonenum);
+            ps.setString(1, username);
+            ps.setString(2, email);
+            ps.setString(3, password);
+            ps.setString(4, phonenum);
             ps.executeUpdate();
         } catch (Exception e) {
         }
@@ -32,7 +35,7 @@ public class UserDAO {
         String query = "SELECT * FROM oj3bOO0Agn.user\n"
                 + "where email = ? and passWord = ?";
         try {
-            conn = new DBContext().getConnection();//mo ket noi voi sql
+            conn = new DBContext().getConnection();
             ps = conn.prepareStatement(query);
             ps.setString(1, email);
             ps.setString(2, password);
@@ -51,19 +54,26 @@ public class UserDAO {
 
     public boolean checkAccountExist(String username, String email) {
         String query = "select * from oj3bOO0Agn.user\n"
-                + "where username = ? and email = ?";
+                + "where userName = ? and email = ?";
         try {
-            conn = new DBContext().getConnection();//mo ket noi voi sql
+            conn = new DBContext().getConnection();
             ps = conn.prepareStatement(query);
             ps.setString(1, username);
             ps.setString(2, email);
             rs = ps.executeQuery();
             while (rs.next()) {
-                return new User(rs.getString(3), rs.getString(4)
-                ) != null;
+                return new User(
+                        rs.getString(2),
+                        rs.getString(3)) != null;
             }
         } catch (Exception e) {
         }
         return false;
+    }
+    public static void main(String[] args) {
+        try {
+            System.out.println(new UserDAO().checkAccountExist("quangbao","quangbaokh9a2@gmail.com"));
+        } catch (Exception e) {
+        }
     }
 }
--- target/generated-sources/annotations/com/bkmovieapplication/entity/Movie_.java
+++ target/generated-sources/annotations/com/bkmovieapplication/entity/Movie_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2022-12-04T14:28:27")
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2022-12-04T16:42:56")
 @StaticMetamodel(Movie.class)
 public class Movie_ { 
 
