59691d7e18d831b36308a754966058163d732e64
Mr-LeeB <quangbaokh9a2@gmail.com>
2022-12-05 03:33:10 +0700

    af

--- target/generated-sources/annotations/com/bkmovieapplication/entity/Movie_.java
+++ target/generated-sources/annotations/com/bkmovieapplication/entity/Movie_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2022-12-05T01:15:40")
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2022-12-05T03:32:51")
 @StaticMetamodel(Movie.class)
 public class Movie_ { 
 
