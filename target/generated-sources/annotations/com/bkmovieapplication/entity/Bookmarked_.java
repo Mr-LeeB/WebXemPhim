25cd3622033df5164a439089442cbad68880eb1b
Mr-LeeB <quangbaokh9a2@gmail.com>
2022-12-14 06:55:15 +0700

    update

--- target/generated-sources/annotations/com/bkmovieapplication/entity/Bookmarked_.java
+++ target/generated-sources/annotations/com/bkmovieapplication/entity/Bookmarked_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2022-12-14T06:43:59")
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2022-12-14T06:55:02")
 @StaticMetamodel(Bookmarked.class)
 public class Bookmarked_ { 
 
--- target/generated-sources/annotations/com/bkmovieapplication/entity/User_.java
+++ target/generated-sources/annotations/com/bkmovieapplication/entity/User_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2022-12-14T06:43:59")
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2022-12-14T06:55:02")
 @StaticMetamodel(User.class)
 public class User_ { 
 
